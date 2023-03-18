44bae23e15f938954053439a5bdb54f3776e3a7b
santhoshT15 <118888754+santhoshT15@users.noreply.github.com>
2023-03-18 15:26:11 +0530

    Add files via upload

---
+++ json resume/script.js
@@ -0,0 +1,43 @@
+let myResume={
+    "basics": {
+      "name": "SANTHOSH T",
+      "degree": "B.E ",
+      "location": {
+        "address": "03, Tindivanam raod,",
+        "postalCode": 604030,
+        "city": "Gingee",
+        "state": "Tamilnadu",
+        "country": "India"
+      },
+      "profiles": [
+        {
+          "github":"https://github.com/santhoshT15"
+        }
+      ]
+    },
+    "education": [
+      {
+        "institution": "VELAMMAL College Of Engineering",
+        "department": "AUTOMOBILE",
+        "studyType": "fulltime",
+        "batch start year": 2016,
+        "batch end year": 2020,
+        "gpa": 7.5,
+      }
+    ],
+    "skills": [
+      {
+        "name": "HTML, CSS,javascript",
+        "level": "beginner",
+        "project": [
+          "Project preview card"-"https://github.com/santhoshT15/Product-preview-card-component"
+        ]
+      }
+    ],
+    "languages": [
+      {
+        "language known": "Tamil,English",
+      }
+    ]
+  }
+  console.log(myResume);
